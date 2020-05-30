57dfa71b71761577849754de30834fc5c9d0c748
AdamiSheff <AdamiSheff@gmail.com>
2020-05-30 14:59:11 -0400

    Updates

---
+++ precache-manifest.3aa32f4849ab56f517c163f441031542.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4011a2702883a008373e0e49ced37df6",
+    "url": "/ToDo/index.html"
+  },
+  {
+    "revision": "67757d33264b704f62e7",
+    "url": "/ToDo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c24e27614ea2ee514e77",
+    "url": "/ToDo/static/css/main.2362c27c.chunk.css"
+  },
+  {
+    "revision": "67757d33264b704f62e7",
+    "url": "/ToDo/static/js/2.c1da8bc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ToDo/static/js/2.c1da8bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24e27614ea2ee514e77",
+    "url": "/ToDo/static/js/main.c1263f68.chunk.js"
+  },
+  {
+    "revision": "88ec3d37b0379201230b",
+    "url": "/ToDo/static/js/runtime-main.3a6dcde6.js"
+  }
+]);
